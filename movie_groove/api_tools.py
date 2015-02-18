b91ba2b5fae4360ede61891eb3bd80ee1a1b710d
Fredrik Ohlin <fohlin@gmail.com>
2015-02-18 18:02:47 +0100

    Updated API functions to return dictionaries as well as normal objects

--- movie_groove/api_tools.py
+++ movie_groove/api_tools.py
@@ -28,7 +28,7 @@ class Track(object):
 
 
 # Returns a movie based on the query
-def get_movie(query):
+def get_movie(query, as_dict = False):
     movie = None
     # HTTP request to IMDB API
     r = urlopen('http://omdbapi.com/?t=' + urlencode(query))
@@ -38,10 +38,13 @@ def get_movie(query):
     if movie_data['Response'] != 'False':
         # Create our Movie object
         movie = Movie(movie_data)
-    return movie
+    if as_dict:
+        return movie.__dict__
+    else:
+        return movie
 
 # Return tracks based on the query
-def get_tracks(query):
+def get_tracks(query, max_tracks = 5, as_dict = False):
     tracks = []
     # HTTP request to Spotify API
     r = urlopen('http://ws.spotify.com/search/1/track.json?q=' + urlencode(query))
@@ -50,9 +53,12 @@ def get_tracks(query):
     # Check that we recieved some tracks
     if len(tracks_data['tracks']) > 0:
         # Get the first 5 tracks
-        for track in tracks_data['tracks'][:5]:
+        for track in tracks_data['tracks'][:max_tracks]:
             # Append a Track object to the list of tracks
-            tracks.append(Track(track))
-
+            if as_dict:
+                tracks.append(Track(track).__dict__)
+            else:
+                tracks.append(Track(track))
+    
     return tracks
 
